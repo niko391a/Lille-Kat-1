82f89a184463ebfd6ca8ccb64a4632d4fea0adeb
niko391a <69769473+niko391a@users.noreply.github.com>
2024-03-15 16:27:23 +0100

    Problem a Done

---
+++ Lille-Kat-1/src/ProblemA/Main.java
@@ -0,0 +1,10 @@
+package ProblemA;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(scanner.nextInt()-1);
+    }
+}
