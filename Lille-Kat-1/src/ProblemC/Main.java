7c24326dfe2117c0b9414e0329b1e987f7e0a265
niko391a <69769473+niko391a@users.noreply.github.com>
2024-03-15 16:40:00 +0100

    Problem C

---
+++ Lille-Kat-1/src/ProblemC/Main.java
@@ -0,0 +1,16 @@
+package ProblemC;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        char[] characters = new char[input.length()];
+        characters = input.toCharArray();
+
+        for(int i = 1 ; i <= characters.length ; i++){
+            if(){
+        }
+    }
+}
