08d2e513bfc62a62b6b509aad9a41ad62ce1b95f
niko391a <69769473+niko391a@users.noreply.github.com>
2024-03-15 16:53:46 +0100

    Problem C done

--- Lille-Kat-1/src/ProblemC/Main.java
+++ Lille-Kat-1/src/ProblemC/Main.java
@@ -6,11 +6,20 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
+        String champerString = "";
         char[] characters = new char[input.length()];
         characters = input.toCharArray();
+        String lastDigit = String.valueOf(characters[characters.length-1]);
 
+        // Creates correct Champerowne string
         for(int i = 1 ; i <= characters.length ; i++){
-            if(){
+            champerString = champerString + i;
+        }
+        // checks if given input matches the correct champerowne string
+        if(champerString.equals(input)){
+            System.out.println(lastDigit);
+        } else {
+            System.out.println(-1);
         }
     }
 }
